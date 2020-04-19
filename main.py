a183408c4591386b9481342a133d50c4b5e3adf3
novahtml <novahtml@yandex.ru>
2020-04-19 23:53:31 +0300

    notebook

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ print('Первые пять строк:\n', data.head())
 #Это общее их количество (count), среднее значение (mean), стандартное отклонение (std), 
 #минимальное (min), макcимальное (max) значения, медиана (50%) и значения нижнего (25%) 
 #и верхнего (75%) квартилей:
-print(data.describe())
+print(data.describe(), '\n')
 
 #Категориальные признаки
 categorical_columns = [c for c in data.columns if data[c].dtype.name == 'object']
-print(categorical_columns)
+print('Категориальные признаки:',categorical_columns, '\n')
 
 #Числовые признаки
 numerical_columns   = [c for c in data.columns if data[c].dtype.name != 'object']
-print(numerical_columns)
+print('Числовые признаки:',numerical_columns)
